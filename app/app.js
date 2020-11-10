422b2b644b58075387a0f9a2686a98231733ec4d
TIKJA <najaroen_tik@live.com>
2020-11-10 16:23:29 +0700

    add-router

--- app/app.js
+++ app/app.js
@@ -7,5 +7,8 @@ app.get('/', (req, res, next) => {
 app.get('/test', (req, res, next) => {
     res.send("Hello Express")
 })
+app.get('/najaroen', (req, res, next) => {
+    res.send("Hello najaroen")
+})
 
 app.listen(3000, () => { console.log('running...')})
